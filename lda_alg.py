aea46708b8e1b34ea025b8d460a735b4c02cb773
Gabriela Bochenska <gbochenska@student.agh.edu.pl>
2023-10-08 12:07:52 +0200

    lda beginning

--- lda_alg.py
+++ lda_alg.py
@@ -5,16 +5,11 @@ import matplotlib.pyplot as plt
 import numpy as np
  
 from sklearn.preprocessing import MinMaxScaler, StandardScaler, LabelEncoder
-from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.model_selection import train_test_split
 
 from data_preprocessing import data_numeric
 
-import sys
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_DIR)
 
 df = pd.read_csv('datasets/heart_2020_cleaned.csv')
 df = data_numeric(df)
@@ -31,28 +26,25 @@ target = df['HeartDisease']
 features = df.drop('HeartDisease',axis=1)
 stand = standarization(df, target, features)
 
-def pca_algorithm(df, components):
-    pca = PCA(n_components = components)
-    transformed_data = pca.fit_transform(df.drop('HeartDisease', axis=1))
+def lda_algorithm(df, components):
 
-    X_train, X_test, y_train, y_test = train_test_split(transformed_data, target, stratify=target, test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(features, target, stratify=target, test_size=0.2)
+    lda = LDA(n_components=components)
+    X_train = lda.fit_transform(X_train, y_train)
+    X_test = lda.transform(X_test)
+
     sns.scatterplot(x=X_train[:,0], y=X_train[:,1], hue=y_train)
     plt.show()
     # eigenvalues that explain the variance in the data
-    explained_variance = pca.explained_variance_
+    explained_variance = lda.explained_variance_
 
     # Eigenvectors (principal components)
-    components = pca.components_
+    components = lda.components_
 
     # Transformed data
-    transformed_data = pca.transform(df)
+    transformed_data = lda.transform(df)
 
     return explained_variance, components, transformed_data
 
 
-
-explained_variance, components, transformed_data = pca_algorithm(stand, 2)
-plt.plot(np.cumsum(explained_variance) / np.sum(explained_variance))
-plt.xlabel('Number of components')
-plt.ylabel('Cumulative explained variance')
-plt.show()
+explained_variance, components, transformed_data = lda_algorithm(stand, 2)
