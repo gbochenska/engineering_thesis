52471afd2e6cef50a5d3aee0af51a0e8a7a449fa
Gabriela Bochenska <gbochenska@student.agh.edu.pl>
2023-10-17 22:33:51 +0200

    correction preprocessing

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,78 +1,69 @@
 import pandas as pd 
 import numpy as np
-import time
 import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 
+"""Data Preprocessing involves below steps:
 
+Getting the dataset
+Importing libraries
+Importing datasets
+Finding Missing Data
+Encoding Categorical Data"""
 
 #Load dataset
 df = pd.read_csv('datasets/heart_2020_cleaned.csv')
-# print(df.head())
 
+# print(df.head())
 # print(df.shape)
 
 #Check the data info
 # print(df.info())
 
 #Check the null values
-df.isnull()
+# df.isnull()
 
 #descriptive overview
-df.describe()
+# df.describe()
 
 
-#heatmap
-# data1 = df[df['Sex'] == 'Female'][['Smoking','Age_Category','HeartDisease']]
-# data1 = data1.pivot('Smoking','Age_Category','HeartDisease')
-# data1.head(3)
-# sns.set(rc={'figure.figsize':(10,8)})
-# ax = sns.heatmap(df)
-
-
-
-
-# objects -> float
+"""Encoding Categorical Data - objects -> float
+Label Encoding Scheme
+One-Hot-Encoding"""
 # Check the data types of the columns
 # print(df.dtypes)
-
 def data_numeric(df):
     age = sorted(df["AgeCategory"].unique())
     # age = ['18-24', '25-29', '30-34', '35-39', '40-44', '45-49', '50-54', '55-59', '60-64', '65-69', '70-74', '75-79', '80 or older']
-    race = sorted(df['Race'].unique())
-    # race = ['American Indian/Alaskan Native', 'Asian', 'Black', 'Hispanic', 'Other', 'White']
     health = ['Poor', 'Fair', 'Good', 'Very good', 'Excellent']
+    df = pd.get_dummies(df, columns=['Race', ], dtype=int)
     #change
     for o in df.columns:
-        if df[o].dtype != 'float64':
-            if df[o][0] in ['Yes', 'No']:
-                df[o] = df[o].map({'Yes': 1.0, 'No': 0.0})
-            elif df[o][0] in ['Male', 'Female']:
-                df[o] = df[o].map({'Male': 1.0, 'Female': 0.0})
-            elif df[o][0] in age:
-                df[o] = df[o].map({'18-24':1.0, '25-29':2.0, '30-34':3.0, '35-39':4.0, '40-44':5.0, '45-49':6.0, '50-54':7.0, '55-59':8.0, '60-64':9.0, '65-69':10.0, '70-74':11.0, '75-79':12.0, '80 or older':13.0})
-            elif df[o][0] in race:
-                    # one hot coding is better?
-                df[o] = df[o].map({'American Indian/Alaskan Native':1.0, 'Asian':2.0, 'Black':3.0, 'Hispanic':4.0, 'Other':5.0, 'White':6.0})
-            elif df[o][0] in health:
-                df[o] = df[o].map({'Poor':1.0, 'Fair':2.0, 'Good':3.0, 'Very good':4.0, 'Excellent':5.0})
-            else:
-                pass
+        if df[o][0] in ['Yes', 'No']:
+            df[o] = df[o].map({'Yes': 1.0, 'No': 0.0})
+        elif df[o][0] in ['Male', 'Female']:
+            df[o] = df[o].map({'Male': 1.0, 'Female': 0.0})
+        elif df[o][0] in age:
+            df[o] = df[o].map({'18-24':1.0, '25-29':2.0, '30-34':3.0, '35-39':4.0, '40-44':5.0, '45-49':6.0, '50-54':7.0, '55-59':8.0, '60-64':9.0, '65-69':10.0, '70-74':11.0, '75-79':12.0, '80 or older':13.0})
+        elif df[o][0] in health:
+            df[o] = df[o].map({'Poor':1.0, 'Fair':2.0, 'Good':3.0, 'Very good':4.0, 'Excellent':5.0})
+        else:
+            pass
         df.dropna(inplace=True)
     return df
 
-df = data_numeric(df)
-print(df.head())
-
-print(df["HeartDisease"].value_counts())
+#check the dataset
+df_numeric = data_numeric(df)
+# print(df_numeric.head())
+# print(df.info())
+print(df_numeric["HeartDisease"].value_counts())
 
 #correlation matrix and heatmap
-cor_matrix = df.corr().abs()
-print(cor_matrix)
+def headmap(df):
+    cor_matrix = df.corr().abs()
+    fig, ax = plt.subplots(figsize=(18,18))
+    dataplot = sns.heatmap(df.corr().abs(), cmap="YlGnBu", annot=True, annot_kws={'size': 10}, ax=ax)
+    fig.show()
 
-fig, ax = plt.subplots(figsize=(18,18))
-dataplot = sns.heatmap(df.corr().abs(), cmap="YlGnBu", annot=True, annot_kws={'size': 10}, ax=ax)
-plt.show()
-
-print(df.head())
+df_numeric.to_csv("datasets/data_after_preprocessing.csv")
--- eda.py
+++ eda.py
@@ -4,27 +4,29 @@ import seaborn as sns
 
 df = pd.read_csv('datasets/heart_2020_cleaned.csv')
 
-X = df.drop(columns='HeartDisease')
-y = pd.DataFrame(df['HeartDisease'])
+print(df.mean())
+print(df.median())
+# X = df.drop(columns='HeartDisease')
+# y = pd.DataFrame(df['HeartDisease'])
 
-#distribution of data in each class chart
-df.hist()
-plt.show()
+# #distribution of data in each class chart
+# df.hist()
+# plt.show()
 
-sns.countplot(x="HeartDisease", data=y)
-plt.title("Do you have a heart disease?")
+# sns.countplot(x="HeartDisease", data=y)
+# plt.title("Do you have a heart disease?")
 
-category = ['Smoking', 'AlcoholDrinking', 'Stroke', 'DiffWalking', 'Sex', 
-            'PhysicalActivity', 'Asthma',
-            'KidneyDisease', 'SkinCancer', 'AgeCategory', 'Race',
-            'Diabetic', 'GenHealth', 'SleepTime']
+# category = ['Smoking', 'AlcoholDrinking', 'Stroke', 'DiffWalking', 'Sex', 
+#             'PhysicalActivity', 'Asthma',
+#             'KidneyDisease', 'SkinCancer', 'AgeCategory', 'Race',
+#             'Diabetic', 'GenHealth', 'SleepTime']
 
-plt.subplots(figsize=(15,40))
-for i,column in  enumerate(category):
-    plt.subplot(len(category), 1, i+1)
-    plt.suptitle("Distribution of data in each class chart", fontsize=20, x=0.5, y=1)
-    sns.countplot(data=df, x=column)
-    plt.title(f"{column}")
-    plt.tight_layout()
-    plt.xticks([])
-plt.show()
+# plt.subplots(figsize=(15,40))
+# for i,column in  enumerate(category):
+#     plt.subplot(len(category), 1, i+1)
+#     plt.suptitle("Distribution of data in each class chart", fontsize=20, x=0.5, y=1)
+#     sns.countplot(data=df, x=column)
+#     plt.title(f"{column}")
+#     plt.tight_layout()
+#     plt.xticks([])
+# plt.show()
