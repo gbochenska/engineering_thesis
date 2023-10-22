2a6182a0014e9888aa5d7b6b41678255bb77aa1d
Gabriela Bochenska <gbochenska@student.agh.edu.pl>
2023-10-22 17:16:16 +0200

    models beginning

---
+++ models.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+def evaluate_model(model, x_test, y_test):
+    from sklearn import metrics
+
+    # Predict Test Data 
+    y_pred = model.predict(x_test)
+
+    # Calculate accuracy, precision, recall, f1-score, and kappa score
+    acc = metrics.accuracy_score(y_test, y_pred)
+    prec = metrics.precision_score(y_test, y_pred)
+    rec = metrics.recall_score(y_test, y_pred)
+    f1 = metrics.f1_score(y_test, y_pred)
+    kappa = metrics.cohen_kappa_score(y_test, y_pred)
+
+    # Calculate area under curve (AUC)
+    y_pred_proba = model.predict_proba(x_test)[::,1]
+    fpr, tpr, _ = metrics.roc_curve(y_test, y_pred_proba)
+    auc = metrics.roc_auc_score(y_test, y_pred_proba)
+
+    # Display confussion matrix
+    cm = metrics.confusion_matrix(y_test, y_pred)
+
+    return {'acc': acc, 'prec': prec, 'rec': rec, 'f1': f1, 'kappa': kappa, 
+            'fpr': fpr, 'tpr': tpr, 'auc': auc, 'cm': cm}
+
+df = pd.read_csv('datasets/data_after_preprocessing.csv')
+df = df.drop('Unnamed: 0',axis=1)
+
+y = df['HeartDisease']
+X = df.drop('HeartDisease',axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle = True, test_size = .2, random_state = 44)
+scaler = StandardScaler()
+
+# Scale trainint data
+X_train = scaler.fit_transform(X_train)
+
+# Scale test data
+X_test = scaler.fit_transform(X_test)
+knn = KNeighborsClassifier(n_neighbors = 5)
+
+knn.fit(X_train, y_train)
+
+
+# Evaluate Model
+knn_eval = evaluate_model(knn, X_test, y_test)
+
+# Print result
+print('Accuracy:', knn_eval['acc'])
+print('Precision:', knn_eval['prec'])
+print('Recall:', knn_eval['rec'])
+print('F1 Score:', knn_eval['f1'])
+print('Cohens Kappa Score:', knn_eval['kappa'])
+print('Area Under Curve:', knn_eval['auc'])
+print('Confusion Matrix:\n', knn_eval['cm'])
