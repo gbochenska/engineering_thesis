f93a2c847ea1571cdb198e336155791653b85428
Gabriela Bochenska <gbochenska@student.agh.edu.pl>
2023-11-07 11:31:46 +0100

    lasso

--- models.py
+++ models.py
@@ -5,11 +5,14 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split, KFold, cross_val_score, GridSearchCV
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.linear_model import Lasso
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 import numpy as np
+from imblearn.over_sampling import RandomOverSampler
 # import xgboost as xgb
+from sklearn.metrics import mean_squared_error
 
 def evaluate_model(model, x_test, y_test):
     from sklearn import metrics
@@ -34,6 +37,18 @@ def evaluate_model(model, x_test, y_test):
 
     return {'acc': acc, 'prec': prec, 'rec': rec, 'f1': f1, 'kappa': kappa, 
             'fpr': fpr, 'tpr': tpr, 'auc': auc, 'cm': cm}
+
+
+def cor_selector(X, y,num_feats='all'):
+    cor_list = []
+    feature_name = X.columns.tolist()
+    for i in X.columns.tolist():
+        cor = np.corrcoef(X[i], y)[0, 1]
+        cor_list.append(cor)
+    cor_list = [0 if np.isnan(i) else i for i in cor_list]
+    cor_feature = X.iloc[:,np.argsort(np.abs(cor_list))[-num_feats:]].columns.tolist()
+    cor_support = [True if i in cor_feature else False for i in feature_name]
+    return cor_support, cor_feature
 
 df = pd.read_csv('datasets/data_after_preprocessing.csv')
 df = df.drop('Unnamed: 0',axis=1)
@@ -97,33 +112,23 @@ def pca_algorithm(X, n_components=None):
 
 
 # # DecisionTree
-def cor_selector(X, y,num_feats='all'):
-    cor_list = []
-    feature_name = X.columns.tolist()
-    for i in X.columns.tolist():
-        cor = np.corrcoef(X[i], y)[0, 1]
-        cor_list.append(cor)
-    cor_list = [0 if np.isnan(i) else i for i in cor_list]
-    cor_feature = X.iloc[:,np.argsort(np.abs(cor_list))[-num_feats:]].columns.tolist()
-    cor_support = [True if i in cor_feature else False for i in feature_name]
-    return cor_support, cor_feature
 
-cor_support, columns = cor_selector(X, y, num_feats=4)
-print(columns)
-X = df[columns]
-y = df['HeartDisease']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 30)
+# cor_support, columns = cor_selector(X, y, num_feats=4)
+# print(columns)
+# X = df[columns]
+# y = df['HeartDisease']
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 30)
 
 
-param_grid = {
-    'criterion': ['gini', 'entropy', 'log_loss'],
-    'splitter': ['best', 'random'],
-    'max_depth': [None, 10,15]
-}
+# param_grid = {
+#     'criterion': ['gini', 'entropy', 'log_loss'],
+#     'splitter': ['best', 'random'],
+#     'max_depth': [None, 10,15]
+# }
 
-model = DecisionTreeClassifier()
-model.fit(X_train, y_train)
-eval = evaluate_model(model, X_test, y_test)
+# model = DecisionTreeClassifier()
+# model.fit(X_train, y_train)
+# eval = evaluate_model(model, X_test, y_test)
 
 # grid_search = GridSearchCV(model, param_grid, cv=5, scoring='accuracy')
 # grid_search.fit(X_train, y_train)
@@ -136,23 +141,53 @@ eval = evaluate_model(model, X_test, y_test)
 
 
 # RandomForest
-# model = RandomForestClassifier()
+# randomOverSample to deal with imbalanced data
+# ros = RandomOverSampler(random_state=42)
+# x_ros, y_ros = ros.fit_resample(X, y)
+# X_train, X_test, y_train, y_test = train_test_split(x_ros, y_ros, test_size=0.2, random_state=30)
+
+# # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 30)
+# scaler = StandardScaler()
+# X_train = scaler.fit_transform(X_train)
+# X_test = scaler.fit_transform(X_test)
+# model = RandomForestClassifier(random_state=30)
 # model.fit(X_train, y_train)
-# eval = evaluate_model(rf, X_test, y_test)
+# eval = evaluate_model(model, X_test, y_test)
 
 # LogisticRegression
 
-#cross validation
+# #cross validation
 
-# kf = KFold(n_splits = 5)
-# for train_index, test_index in kf.split(X.values):
-#     print("TRAIN:", train_index, "TEST:", test_index)
-#     X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-#     y_train, y_test = y[train_index], y[test_index]
+# # kf = KFold(n_splits = 5)
+# # for train_index, test_index in kf.split(X.values):
+# #     print("TRAIN:", train_index, "TEST:", test_index)
+# #     X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+# #     y_train, y_test = y[train_index], y[test_index]
 
-# model = LogisticRegression(random_state=16)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 30)
+
+# model = LogisticRegression()
 # model.fit(X_train, y_train)
 # eval = evaluate_model(model, X_test, y_test)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 30)
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.fit_transform(X_test)
+from sklearn.linear_model import LassoCV
+
+# Lasso with 5 fold cross-validation
+model = LassoCV(cv=5, random_state=0, max_iter=10000)
+model.fit(X_train, y_train)
+print(model.alpha_)
+
+model = Lasso(model.alpha_)
+model.fit(X_train, y_train)
+print("Train Set R-square Val: {:.3f}".format(model.score(X_train, y_train)))
+print("Test Set R-square Val: {:.3f}".format(model.score(X_test, y_test)))
+print(mean_squared_error(y_test, model.predict(X_test)))
+print(mean_squared_error(y_train, model.predict(X_train)))
 
 # Print result
 print(model)
