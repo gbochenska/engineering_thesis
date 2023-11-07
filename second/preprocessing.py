1795f9053ee5133417b8bc2a4bb9b59c29a782d3
Gabriela Bochenska <gbochenska@student.agh.edu.pl>
2023-11-07 22:08:06 +0100

    second dataset

---
+++ second/preprocessing.py
@@ -0,0 +1,182 @@
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+xpt_file = "C:/Users/gboch/Desktop/inzynierka/engineering_thesis/second/datasets/LLCP2022.XPT"
+# df = pd.read_sas(xpt_file, encoding='utf-8')
+# df.to_csv('C:/Users/gboch/Desktop/inzynierka/engineering_thesis/second/datasets/new.csv', index=False)
+df = pd.read_csv('datasets/new.csv')
+print(df.head())
+
+NEW_VAR_NAMES = [
+    "HeartDisease", 
+    "BMI", 
+    "Smoking", 
+    "AlcoholDrinking", 
+    "Stroke",
+    "PhysicalHealth", 
+    "MentalHealth",
+    "DiffWalking",
+    "Sex",
+    "AgeCategory",
+    "Race",
+    "Diabetic",
+    "PhysicalActivity",
+    "GenHealth",
+    "SleepTime",
+    "Asthma",
+    "KidneyDisease",
+    "SkinCancer",
+    "State"
+]
+
+var_list = [
+    "_MICHD", 
+    "_BMI5",
+    "_SMOKER3",  
+    "DRNKANY6",
+    "CVDSTRK3", 
+    "PHYSHLTH", 
+    "MENTHLTH",
+    "DIFFWALK",
+    "_SEX", 
+    "_AGEG5YR",
+    "_RACEGR4",
+    "DIABETE4",
+    "EXERANY2",
+    "GENHLTH", 
+    "SLEPTIM1",
+    "ASTHMA3",
+    "CHCKDNY2", 
+    "CHCSCNC1",  
+    "_STATE"]
+
+heart_df = df[var_list]
+heart_df.columns = NEW_VAR_NAMES
+
+STATE = {
+    1: "Alabama",
+    2: "Alaska",
+    4: "Arizona",
+    5: "Arkansas",
+    6: "California",
+    8: "Colorado",
+    9: "Connecticut",
+    10: "Delaware",
+    11: "District of Columbia",
+    12: "Florida",
+    13: "Georgia",
+    15: "Hawaii",
+    16: "Idaho",
+    17: "Illinois",
+    18: "Indiana",
+    19: "Iowa",
+    20: "Kansas",
+    21: "Kentucky",
+    22: "Louisiana",
+    23: "Maine",
+    24: "Maryland",
+    25: "Massachusetts",
+    26: "Michigan",
+    27: "Minnesota",
+    28: "Mississippi",
+    29: "Missouri",
+    30: "Montana",
+    31: "Nebraska",
+    32: "Nevada",
+    33: "New Hampshire",
+    34: "New Jersey",
+    35: "New Mexico",
+    36: "New York",
+    37: "North Carolina",
+    38: "North Dakota",
+    39: "Ohio",
+    40: "Oklahoma",
+    41: "Oregon",
+    42: "Pennsylvania",
+    44: "Rhode Island",
+    45: "South Carolina",
+    46: "South Dakota",
+    47: "Tennessee",
+    48: "Texas",
+    49: "Utah",
+    50: "Vermont",
+    51: "Virginia",
+    53: "Washington",
+    54: "West Virginia",
+    55: "Wisconsin",
+    56: "Wyoming",
+    66: "Guam",
+    72: "Puerto Rico",
+    78: "Virgin Islands"
+}
+
+SEX = {1: 'Male', 2: 'Female'}
+
+GEN_HEALTH = {
+    1: "Excellent",
+    2: "Very good",
+    3: "Good",
+    4: "Fair",
+    5: "Poor"
+}
+
+YES_NO_QUESTIONS = {1: 'Yes', 2: 'No'}
+
+SLEEP_TIME = lambda x: np.where(x > 24, np.nan, x)
+PHYS_MEN_HEALTH = lambda x: np.where(x > 31, np.nan, x)
+
+AGE_CATEGORY = {
+    1: "Age 18 to 24",
+    2: "Age 25 to 29",
+    3: "Age 30 to 34",
+    4: "Age 35 to 39",
+    5: "Age 40 to 44",
+    6: "Age 45 to 49",
+    7: "Age 50 to 54",
+    8: "Age 55 to 59",
+    9: "Age 60 to 64",
+    10: "Age 65 to 69",
+    11: "Age 70 to 74",
+    12: "Age 75 to 79",
+    13: "Age 80 or older"
+}
+
+SMOKER_STATUS = {
+    1: "Yes",
+    2: "Yes",
+    3: "No",
+    4: "No"
+}
+
+RACE = {
+    1: "White",
+    2: "Black",
+    3: "Other",
+    4: "Multiracial",
+    5: "Hispanic"
+}
+
+heart_copy_df = heart_df.copy()
+heart_copy_df['State'] = heart_copy_df['State'].map(STATE)
+heart_copy_df['Sex'] = heart_copy_df['Sex'].map(SEX)
+heart_copy_df['GenHealth'] = heart_copy_df['GenHealth'].map(GEN_HEALTH)
+heart_copy_df['PhysicalHealth'] = heart_copy_df['PhysicalHealth'].map(PHYS_MEN_HEALTH)
+heart_copy_df['MentalHealth'] = heart_copy_df['MentalHealth'].map(PHYS_MEN_HEALTH)
+heart_copy_df['PhysicalActivity'] = heart_copy_df['PhysicalActivity'].map(YES_NO_QUESTIONS)
+heart_copy_df['SleepTime'] = heart_copy_df['SleepTime'].map(SLEEP_TIME)
+heart_copy_df['HeartDisease'] = heart_copy_df['HeartDisease'].map(YES_NO_QUESTIONS)
+heart_copy_df['Stroke'] = heart_copy_df['Stroke'].map(YES_NO_QUESTIONS)
+heart_copy_df['Asthma'] = heart_copy_df['Asthma'].map(YES_NO_QUESTIONS)
+heart_copy_df['SkinCancer'] = heart_copy_df['SkinCancer'].map(YES_NO_QUESTIONS)
+heart_copy_df['KidneyDisease'] = heart_copy_df['KidneyDisease'].map(YES_NO_QUESTIONS)
+heart_copy_df['DiffWalking'] = heart_copy_df['DiffWalking'].map(YES_NO_QUESTIONS)
+heart_copy_df['Smoking'] = heart_copy_df['Smoking'].map(SMOKER_STATUS)
+heart_copy_df['Race'] = heart_copy_df['Race'].map(RACE)
+heart_copy_df['AgeCategory'] = heart_copy_df['AgeCategory'].map(AGE_CATEGORY)
+heart_copy_df['BMI'] = heart_copy_df['BMI'] / 100
+heart_copy_df['AlcoholDrinking'] = heart_copy_df['AlcoholDrinking'].map(YES_NO_QUESTIONS)
+
+print(heart_copy_df.head())
+
+heart_copy_df.to_csv('C:/Users/gboch/Desktop/inzynierka/engineering_thesis/second/datasets/new.csv', index=False)
---
+++ second/preprocessing2.py
@@ -0,0 +1,87 @@
+import pandas as pd 
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+"""Data Preprocessing involves below steps:
+
+Getting the dataset
+Importing libraries
+Importing datasets
+Finding Missing Data
+Encoding Categorical Data"""
+
+#Load dataset
+df = pd.read_csv('datasets/heart_2020_cleaned.csv')
+
+# print(df.head())
+# print(df.shape)
+
+#Check the data info
+# print(df.info())
+
+#Check the null values
+# print(df.isnull())
+df = df.dropna()
+print(df.info())
+
+#descriptive overview
+# print(df.describe())
+
+#remove duplicates
+# print(df.duplicated().sum())
+# df = df.drop_duplicates()
+
+# """Encoding Categorical Data - objects -> float
+# Label Encoding Scheme
+# One-Hot-Encoding"""
+# # Check the data types of the columns
+# # print(df.dtypes)
+# def data_numeric(df):
+#     age = sorted(df["AgeCategory"].unique())
+#     # age = ['18-24', '25-29', '30-34', '35-39', '40-44', '45-49', '50-54', '55-59', '60-64', '65-69', '70-74', '75-79', '80 or older']
+#     health = ['Poor', 'Fair', 'Good', 'Very good', 'Excellent']
+#     df = pd.get_dummies(df, columns=['Race', ], dtype=int)
+#     #change
+#     for o in df.columns:
+#         if df[o][0] in ['Yes', 'No']:
+#             df[o] = df[o].map({'Yes': 1.0, 'No': 0.0})
+#         elif df[o][0] in ['Male', 'Female']:
+#             df[o] = df[o].map({'Male': 1.0, 'Female': 0.0})
+#         elif df[o][0] in age:
+#             df[o] = df[o].map({'18-24':1.0, '25-29':2.0, '30-34':3.0, '35-39':4.0, '40-44':5.0, '45-49':6.0, '50-54':7.0, '55-59':8.0, '60-64':9.0, '65-69':10.0, '70-74':11.0, '75-79':12.0, '80 or older':13.0})
+#         elif df[o][0] in health:
+#             df[o] = df[o].map({'Poor':1.0, 'Fair':2.0, 'Good':3.0, 'Very good':4.0, 'Excellent':5.0})
+#         else:
+#             pass
+#         df.dropna(inplace=True)
+#     return df
+
+# #check the dataset
+# df_numeric = data_numeric(df)
+# # print(df_numeric.head())
+# # print(df.info())
+# print(df_numeric["HeartDisease"].value_counts())
+
+# #correlation matrix and heatmap
+# def headmap(df):
+#     cor_matrix = df.corr().abs()
+#     fig, ax = plt.subplots(figsize=(18,18))
+#     dataplot = sns.heatmap(df.corr().abs(), cmap="YlGnBu", annot=True, annot_kws={'size': 10}, ax=ax)
+#     plt.show()
+
+
+# def standarization(df):
+#     y = df['HeartDisease']
+#     X = df.drop('HeartDisease',axis=1)
+#     # StandardScaler object initialization
+#     scaler = MinMaxScaler()
+#     # standarization
+#     standarized = scaler.fit_transform(X)
+#     df_standarized = pd.DataFrame(standarized, columns=X.columns)
+#     return df_standarized
+
+# df_standarized = standarization(df_numeric)
+
